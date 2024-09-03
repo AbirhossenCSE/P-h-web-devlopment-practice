b5114b3c7fb7bdf15a9f8c9018eb0006f8b9c0be
Abir Hossen <abir.cse4@gmail.com>
2024-09-03 13:05:36 +0600

    JS

--- JS/veriable.js
+++ JS/veriable.js
@@ -20,9 +20,25 @@ console.log(typeof country);
 
 var orange = 20;
 var chocolate = 0.5;
-var apple = 20;
+var apple = parseInt('100');
 
 console.log(orange + apple);
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total.toFixed(2));
+
+
+var price = 35;
+price += 10;
+console.log(price);
+
+var price2 = 3000;
+price2 = price2 - 1000;
+console.log(price2);
+
 
 
 
