a7152ffde7bce74c66c809a4b520ea1407b94e20
Abir Hossen <abir.cse4@gmail.com>
2024-09-03 11:59:45 +0600

    JS

--- JS/veriable.js
+++ JS/veriable.js
@@ -1,6 +1,31 @@
-var weight = 38;
-var price = 40;
-var year = 2000;
-var money = 5000;
+// var weight = 38;
+// var price = 40;
+// var year = 2000;
+// var money = 5000;
 
-console.log(money);
+// console.log(money);
+
+
+// String type variable
+var name = "Abir Hossen";
+var address = 'Daulotpur Kushtia';
+var country = 'Bangladesh';
+
+console.log(name);
+
+var isRich = true;
+console.log(isRich);
+
+console.log(typeof country);
+
+var orange = 20;
+var chocolate = 0.5;
+var apple = 20;
+
+console.log(orange + apple);
+
+
+
+
+
+
