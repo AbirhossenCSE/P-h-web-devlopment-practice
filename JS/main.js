addd8e6bca76daf1a07a405419148e2658ae0166
Abir Hossen <abir.cse4@gmail.com>
2024-09-03 11:13:43 +0600

    JS

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,13 @@
-// let score = 75;
-// let grade;
-// if (score >= 90) {
-//  grade = "A";
-// } else if (score >= 80) {
-//  grade = "B";
-// }
-// console.log(grade);
+let score = 75;
+let grade;
+if (score >= 90) {
+ grade = "A";
+} else if (score >= 80) {
+ grade = "B";
+}
+console.log(grade);
+
+
 
 const time = new Date().getHours();
 let greeting;
@@ -17,3 +19,16 @@ if (time < 10) {
   greeting = "Good evening";
 }
 document.getElementById("demo").innerHTML = greeting;
+
+
+
+console.log(33);
+
+
+
+var weight = 38;
+var price = 40;
+var year = 2000;
+var money = 5000;
+
+console.log(money);
---
+++ JS/veriable.js
@@ -0,0 +1,6 @@
+var weight = 38;
+var price = 40;
+var year = 2000;
+var money = 5000;
+
+console.log(money);
