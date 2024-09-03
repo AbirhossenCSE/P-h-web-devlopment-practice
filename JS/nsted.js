5b9552133d1637843756478a1d25e8bb75c79c67
Abir Hossen <abir.cse4@gmail.com>
2024-09-04 00:04:18 +0600

    JS

---
+++ JS/nsted.js
@@ -0,0 +1,31 @@
+const money = 550;
+if (money > 300) {
+    console.log('you are ricgh');
+    
+} else {
+    if (money > 100) {
+        console.log('not poor');
+        
+    }else{
+        if (money > 0) {
+            console.log('eat banana');
+            
+        }else{
+            console.log('Nikal');
+            
+        }
+    }
+}
+
+const age = 12;
+if (age >= 18) {
+    console.log('You can vote');
+    
+} else {
+    console.log('You cant vote');
+    
+}
+
+
+age >= 18 ? console.log('vote dio') : console.log('ghumi thako');
+
